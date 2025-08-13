7a6df3c631f22832886bf84a3d8fb1db6092adc1
Metaboom4304 <159423683+Metaboom4304@users.noreply.github.com>
2025-08-13 23:58:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// 🔐 Supabase Init
+// 🔐 Supabase Init (анон-ключ)
 const supabase = createClient(
   'https://nysjreargnvyjmcirinp.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55c2pyZWFyZ252eWptY2lyaW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MDgxNDIsImV4cCI6MjA3MDM4NDE0Mn0.UZpiU_nM_ACF8bILAGF4oa-WSHaU38KX6Dtz_srZK9Q'
